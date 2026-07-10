050169cb28312d344afe2ddcc9d03f996798d47c
Yuanataa <nata46723@gmail.com>
2026-07-10 15:20:47 +0700

    uji coba DEMO (harus diperbaiki lagi kelak)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks, Security, Request, Query
+from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks, Security, Request, Query, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader, HTTPBearer, HTTPAuthorizationCredentials
 from slowapi import Limiter, _rate_limit_exceeded_handler
@@ -23,6 +23,8 @@ import secrets
 from typing import List
 import segno
 import io
+"""import cv2
+import numpy as np"""
 import base64
 from functools import lru_cache
 
@@ -313,6 +315,38 @@ def verify_qr_code(qr_token:str, db: Session=Depends(get_db), kunci:str=Depends(
     if not user.is_verified:
         raise HTTPException(status_code=403, detail="Akun belum diverifikasi OTP. Selesaikan verifikasi WhatsApp di web GCM terlebih dahulu sebelum menggunakan Vending Machine")
     return {"status": "Akses diberikan", "npm": user.npm, "nama": user.name, "instruksi_mesin": "Buka pintu masuk botol"}
+
+"""@app.post("/machine/scan-qr-image")
+@limiter.limit("60/minute")
+def scan_qr_image(request: Request, file: UploadFile = File(...), db: Session=Depends(get_db), kunci: str=Depends(machine_validate)):
+    if file.content_type not in ["image/jpeg", "image/jpg"]:
+        raise HTTPException(status_code=400, detail="Format file tidak didukung, harus jpeg/jpg")
+    isi_file=file.file.read()
+    if len(isi_file)==0:
+        raise HTTPException(status_code=400, detail="File Kosong")
+    MAX_SIZE=2*1024*1024
+    if len(isi_file)> MAX_SIZE:
+        raise HTTPException(status_code=413, detail="Ukuran file terlalu besar, maskimal 2MB")
+    
+    np_array=np.frombuffer(isi_file, dtype=np.uint8)
+    gambar=cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+    if gambar is None:
+        raise HTTPException(status_code=400, detail="File bukan gambar yang valid atau rusak")
+    
+    detector= cv2.QRCodeDetector()
+    data_qr, bbox, straight_qrcode=detector.detectAndDecode(gambar)
+    if not data_qr:
+        raise HTTPException(status_code=422, detail="QR code tidak terdeteksi di dalam gambar")
+    
+    user=db.query(User).filter(User.qr_token==data_qr).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="QR tidak valid atau user belum terdaftar. Silahkan melakukan pendaftaran terlebih dahulu melalui web GCM")
+    if not user.is_verified:
+        raise HTTPException(status_code=403, detail="Akun belum diverifikasi OTP. Selesaikan verifikasi OTP terlebih dahulu di Web GCM")
+    
+    return{"status": "Akses diberikan", "npm": user.npm, "nama": user.name, "instruksi_mesin": "Buka pintu masuk botol"} """
+
 
 @app.get("/users/my-qr")
 def get_user_qr(current_user: User=Depends(get_current_user)):
@@ -691,6 +725,60 @@ def send_capacity_alert(machine_id, pct: float):
             print(f"ERROR: Telegram menolak pesan - status {resp.status_code}, response: {resp.text}")
     except Exception as e:
         print(f"ERROR: Gagal mengirim pesan peringatan ke telegram - {e}")
+
+
+from pydantic import BaseModel
+
+# Skema khusus untuk menerima data murni dari ESP32 saat demo
+class DemoDeposit(BaseModel):
+    machine_id: str
+    bottle_small: int = 0
+    bottle_medium: int = 0
+    bottle_large: int = 0
+
+# ==========================================================
+# ENDPOINT KHUSUS DEMO PAMERAN (TANPA QR CODE)
+# ==========================================================
+@app.post("/machine/prototype/deposit", tags=["Machine Prototype"])
+def machine_prototype_deposit(
+    data: DemoDeposit,
+    db: Session = Depends(get_db),
+    authenticated: bool = Depends(machine_validate) 
+):
+    NPM_AKUN_DEMO = "25082010106" 
+    
+    # Cari user di database
+    user = db.query(User).filter(User.npm == NPM_AKUN_DEMO).first()
+    if not user:
+        raise HTTPException(
+            status_code=404, 
+            detail="Akun demo pameran belum terdaftar."
+        )
+    
+    # 2. Kalkulasi Poin
+    poin_kecil = data.bottle_small * settings.HARGA_BOTOL_KECIL
+    poin_sedang = data.bottle_medium * settings.HARGA_BOTOL_SEDANG
+    poin_besar = data.bottle_large * settings.HARGA_BOTOL_BESAR
+    total_poin = poin_kecil + poin_sedang + poin_besar
+    
+    # 3. Masukkan ke Database
+    new_transaction = Transaction(
+        user_id=user.id,
+        bottle_small=data.bottle_small,
+        bottle_medium=data.bottle_medium,
+        bottle_large=data.bottle_large,
+        points=total_poin
+    )
+    db.add(new_transaction)
+    db.commit()
+    db.refresh(new_transaction)
+    
+    return {
+        "status": "Success",
+        "message": f"Data berhasil masuk ke akun {user.name}!",
+        "total_poin": total_poin
+    }
+
 
 @app.post("/iot/machine-status")
 def  update_machine_status(data: MachineStatusUpdate, bg_tasks: BackgroundTasks, db: Session=Depends(get_db), kunci: str=Depends(machine_validate)):
